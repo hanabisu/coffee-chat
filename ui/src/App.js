6cf05447849427cebd3a6c79cd5d85dba6539408
minesher <minesher@students.cs.ubc.ca>
2022-01-15 20:08:06 -0800

    added basic header and groups

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,49 @@
 import React from "react";
-import {Switch, Route, Link} from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Login from "./components/login";
-
+import Groups from "./components/groups";
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  // dummy login system for now
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      hello world.
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/coffee-chats" className="navbar-brand">
+          Coffee Chats
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/groups"} className="nav-link">
+              Groups
+            </Link>
+          </li>
+          <li className="nav-item" >
+            { user ? (
+              <a onClick={logout} className="nav-link" style={{cursor:'pointer'}}>
+                Logout {user.name}
+              </a>
+            ) : (            
+            <Link to={"/login"} className="nav-link">
+              Login
+            </Link>
+            )}
+
+          </li>
+        </div>
+      </nav>
+
+
     </div>
   );
 }
---
+++ ui/src/components/groups.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Groups() {
+    return (
+      <div className="App">
+        hello world.
+      </div>
+    );
+  }
+  
+  export default Groups;
